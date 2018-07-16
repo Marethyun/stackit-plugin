6c9437809d3798784708b9eb53d133b8f32a2d75
Misbeliever <bacci.ange@gmail.com>
2018-07-16 16:32:06 +0200

    Added a simple API listener

---
+++ src/main/java/org/stackit/api/RootListener.java
@@ -0,0 +1,27 @@
+package org.stackit.api;
+
+import io.noctin.configuration.Configuration;
+import io.noctin.configuration.JsonConfiguration;
+import io.noctin.events.Listener;
+import io.noctin.events.Trigger;
+import io.noctin.http.EndPoint;
+import io.noctin.http.HttpGetEvent;
+import io.noctin.network.http.server.renderer.RestEngine;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.stackit.StackIt;
+
+public class RootListener implements Listener {
+    @Trigger @EndPoint("/")
+    public void root(HttpGetEvent e){
+
+        e.response.type("application/json");
+
+        JsonConfiguration configuration = JsonConfiguration.createBlank();
+
+        PluginDescriptionFile description = StackIt.getInstance().description;
+
+        configuration.set("stackit-version", description.getVersion());
+
+        e.render(new RestEngine(configuration).render());
+    }
+}
