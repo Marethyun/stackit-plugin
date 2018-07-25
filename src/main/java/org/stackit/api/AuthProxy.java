442c4deb52c7a482dd9b6976e9dcb9cdd17abe1d
Misbeliever <bacci.ange@gmail.com>
2018-07-25 18:18:42 +0200

    Added authentication proxy

---
+++ src/main/java/org/stackit/api/AuthProxy.java
@@ -0,0 +1,29 @@
+package org.stackit.api;
+
+import io.noctin.events.Event;
+import io.noctin.events.EventProxy;
+import io.noctin.http.HttpEvent;
+import io.noctin.network.http.server.HTTPStatus;
+import io.noctin.network.http.server.renderer.JsonHeaders;
+import io.noctin.network.http.server.renderer.RestEngine;
+
+public class AuthProxy extends EventProxy {
+    @Override
+    public boolean forward(Event event) {
+
+        HttpEvent e = (HttpEvent) event;
+
+        if (e.request.session().attribute(AuthenticationListener.SESSION_ATTRIBUTE_NAME) == null){
+
+            JsonHeaders headers = new JsonHeaders();
+
+            headers.status(HTTPStatus.UNAUTHORIZED);
+
+            e.render(new RestEngine(headers).render());
+
+            return false;
+        }
+
+        return true;
+    }
+}
