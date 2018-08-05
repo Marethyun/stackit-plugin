80eeb7fff2a29201efaea63d2981db53067ce6dc
Misbeliever <bacci.ange@gmail.com>
2018-08-05 16:02:29 +0200

    Added a container class

---
+++ src/main/java/org/stackit/StackItContainer.java
@@ -0,0 +1,10 @@
+package org.stackit;
+
+public abstract class StackItContainer {
+
+    protected final StackIt pluginInstance;
+
+    public StackItContainer(StackIt pluginInstance) {
+        this.pluginInstance = pluginInstance;
+    }
+}
