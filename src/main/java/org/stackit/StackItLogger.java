d97173e5448bcb16d477e1b623dddd363e45b96a
Misbeliever <bacci.ange@gmail.com>
2018-08-05 16:02:40 +0200

    Implementing logger

--- src/main/java/org/stackit/StackItLogger.java
+++ src/main/java/org/stackit/StackItLogger.java
@@ -1,4 +1,63 @@
 package org.stackit;
 
-public class StackItLogger {
+import org.bukkit.ChatColor;
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+
+import static org.stackit.StackItLogger.LevelColors.*;
+
+public final class StackItLogger extends StackItContainer {
+
+    private final String prefix;
+    private final Server server = this.pluginInstance.getServer();
+    private final CommandSender sender;
+
+    public StackItLogger(StackIt pluginInstance, CommandSender sender, String prefix) {
+        super(pluginInstance);
+        this.sender = sender;
+        this.prefix = prefix;
+    }
+
+    public StackItLogger(StackIt pluginInstance, CommandSender sender) {
+        this(pluginInstance, sender, ChatColor.AQUA + "[StackIt] ");
+    }
+
+    public void info(Object message){
+        sender.sendMessage(this.prefix + INFO.color + message);
+    }
+
+    public void warn(Object message){
+        sender.sendMessage(this.prefix + WARN.color + message);
+    }
+
+    public void debug(Object message){
+        sender.sendMessage(this.prefix + DEBUG.color + message);
+    }
+
+    public void error(Object message){
+        sender.sendMessage(this.prefix + ERROR.color + message);
+    }
+
+    public void fatal(Object message){
+        sender.sendMessage(this.prefix + FATAL.color + message);
+    }
+
+    public void success(Object message){
+        sender.sendMessage(this.prefix + SUCCESS.color + message);
+    }
+
+    public enum LevelColors {
+        INFO(ChatColor.AQUA),
+        WARN(ChatColor.YELLOW),
+        DEBUG(ChatColor.DARK_AQUA),
+        ERROR(ChatColor.RED),
+        FATAL(ChatColor.DARK_RED),
+        SUCCESS(ChatColor.GREEN);
+
+        public ChatColor color;
+
+        LevelColors(ChatColor color) {
+            this.color = color;
+        }
+    }
 }
